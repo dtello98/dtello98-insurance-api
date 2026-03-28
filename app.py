2d75ff30676581160a31dfc53cd59aa2df5b8bb2
dtello98 <diegoalonsotelloalva1998@gmail.com>
2026-03-27 19:00:34 -0500

    cambios

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def predict_price(age):
 def index():
     context = {
         'title':'TRABAJO FINAL MOULO 8',
-        'message':'AUTOR : '
+        'message':'AUTOR : DIEGO TELLO '
     }
     return jsonify(context)
 
@@ -90,6 +90,58 @@ def set_data():
     context = data_schema.dump(new_data)
     
     return jsonify(context)
+
+@app.route('/insurance',methods=['GET'])
+def get_data():
+    data = Insurance.query.all() # select * from housing
+    data_schema = InsuranceSchema(many=True)
+    return jsonify(data_schema.dump(data))
+
+@app.route('/insurance/<int:id>',methods=['GET'])
+def get_data_by_id(id):
+    data = Insurance.query.get(id) # select * from housing where id = id
+    data_schema = InsuranceSchema()
+    
+    return jsonify(data_schema.dump(data)),200 if data else 404
+
+@app.route('/insurance/<int:id>',methods=['PUT'])
+def update_data(id):
+    data = Insurance.query.get(id) #select * from housing where id = id
+    if not data:
+        context = {
+            'message':'Registro no encontrado'
+        }
+        return jsonify(context),404
+    
+    age = request.json['age']
+    price = predict_price(age)
+    
+    data.rooms = age
+    data.price = price
+    db.session.commit()
+    
+    data_schema = InsuranceSchema()
+    
+    return jsonify(data_schema.dump(data)),200
+
+@app.route('/insurance/<int:id>',methods=['DELETE'])
+def delete_data(id):
+    data = Insurance.query.get(id)
+    
+    if not data:
+        context = {
+            'message':'Registro no encontrado'
+        }
+        return jsonify(context),404
+    
+    db.session.delete(data) #delete from housing
+    db.session.commit()
+    
+    context = {
+        'message':'Registro eliminado correctamente'
+    }
+    
+    return jsonify(context),200
     
 if __name__ == '__main__':
     app.run(debug=True)
